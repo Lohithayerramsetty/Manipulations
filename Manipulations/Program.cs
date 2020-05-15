e2dde27cdfedb01005830a14a828874b0209d425
Lohitha Yerramsetty <lohitha.yerramsetty@gmail.com>
2020-05-15 17:16:12 +0200

    Manipulations

---
+++ Manipulations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Manipulations
+{
+    class Programṇ
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Type the sentence");
+            String sentence = Console.ReadLine();
+            String SpacesToUnderScore = sentence.Replace(" ", "_");
+            string removeLeadingSpaces = sentence.TrimEnd(' ');
+            string removetrailingspaces = sentence.TrimStart(' ');
+            String lowerToUppercaseA = sentence. Replace("a", " A");
+
+
+            Console.WriteLine("Underscore instead of spaces: " + SpacesToUnderScore );
+            Console.WriteLine("Without leading spaces:" + removeLeadingSpaces );
+            Console.WriteLine("Without trailing spaces:" + removetrailingspaces);
+            Console.WriteLine("a to A" + lowerToUppercaseA);
+
+            Console.ReadLine();
+
+        }
+    }
+}
